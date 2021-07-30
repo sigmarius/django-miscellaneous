c3aa2a095d55fd476b16ae4804e80ab864e16ce4
sigmarius <aamokeeva@yandex.ru>
2021-07-30 12:40:22 +0300

    Uses forms, associated with data models, user validation

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -30,14 +30,11 @@ def about(request):
 
 def addpost(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Where was a mistake during adding post in the Dstabase')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
     return render(request, 'women/addpost.html', {'form': form, 'menu': menu, 'title': "Adding post"})

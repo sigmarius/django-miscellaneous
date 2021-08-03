4713ea5335cd2e2249653683f09c062f6eb13b8b
sigmarius <aamokeeva@yandex.ru>
2021-08-03 14:27:26 +0300

    Optimizing SQL with Django Debug Toolbar

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -26,7 +26,7 @@ class WomenHome(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
-        return Women.objects.filter(is_published=True)
+        return Women.objects.filter(is_published=True).select_related('cat')
 
 
 @login_required
@@ -78,12 +78,14 @@ class WomenCategory(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Category Page - ' + str(context['posts'][0].cat),
-                                      cat_selected=context['posts'][0].cat_id)
+
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Category Page - ' + str(c.name),
+                                      cat_selected=c.pk)
         return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
-        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
 
 
 class RegisterUser(DataMixin, CreateView):

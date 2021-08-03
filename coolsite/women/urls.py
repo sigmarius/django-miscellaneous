3d04beacb424a6978f1530f44f8b6ecd474ff87e
sigmarius <aamokeeva@yandex.ru>
2021-08-03 15:29:04 +0300

    Enabling data caching

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
 urlpatterns = [
     # http://127.0.0.1:8080/
+    # path('', cache_page(60)(WomenHome.as_view()), name='home'),  # 60seconds cached
     path('', WomenHome.as_view(), name='home'),
     path('about/', about, name='about'),
     path('addpost/', AddPost.as_view(), name='add_post'),
--- coolsite/women/utils.py
+++ coolsite/women/utils.py
@@ -1,4 +1,6 @@
 from django.db.models import Count
+from django.core.cache import cache
+
 from .models import *
 
 menu = [{'title': 'About our site', 'url_name': 'about'},
@@ -12,7 +14,11 @@ class DataMixin:
 
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(Count('women'))
+
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('women'))
+            cache.set('cats', cats, 60)
 
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
